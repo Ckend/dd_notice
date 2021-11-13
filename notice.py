233d37ac0e14dde5558158e480831b0f6ef33271
ckend <83493903@qq.com>
2021-11-13 19:03:34 +0800

    optimization: 优化写法

--- notice.py
+++ notice.py
@@ -12,38 +12,37 @@ from urllib.parse import quote_plus
 
 class Messenger:
     def __init__(self, token=os.getenv("DD_ACCESS_TOKEN"), secret=os.getenv("DD_SECRET")):
-        self.__token = token
         self.timestamp = str(round(time.time() * 1000))
+        self.URL = "https://oapi.dingtalk.com/robot/send"
+        self.headers = {'Content-Type': 'application/json'}
         secret = secret
         secret_enc = secret.encode('utf-8')
         string_to_sign = '{}\n{}'.format(self.timestamp, secret)
         string_to_sign_enc = string_to_sign.encode('utf-8')
         hmac_code = hmac.new(secret_enc, string_to_sign_enc, digestmod=hashlib.sha256).digest()
         self.sign = quote_plus(base64.b64encode(hmac_code))
-        self.URL = "https://oapi.dingtalk.com/robot/send"
+        self.params = {'access_token': token, "sign": self.sign}
 
     def send_text(self, content):
         data = {"msgtype": "text", "text": {"content": content}}
-        params = {'access_token': self.__token, "sign": self.sign, "timestamp": self.timestamp}
-        headers = {'Content-Type': 'application/json'}
+        self.params["timestamp"] = self.timestamp
         return requests.post(
             url=self.URL,
             data=json.dumps(data),
-            params=params,
-            headers=headers
+            params=self.params,
+            headers=self.headers
         )
 
     def send_md(self, title, content):
         data = {"msgtype": "markdown", "markdown": {"title": title, "text": content}}
-        params = {'access_token': self.__token, "sign": self.sign, "timestamp": self.timestamp}
-        headers = {'Content-Type': 'application/json'}
+        self.params["timestamp"] = self.timestamp
         return requests.post(
             url=self.URL,
             data=json.dumps(data),
-            params=params,
-            headers=headers
+            params=self.params,
+            headers=self.headers
         )
-
+        
 
 if __name__ == "__main__":
     markdown_text = "\n".join(open("md_test.md", encoding="utf-8").readlines())
